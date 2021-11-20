63ad18f7a8e7c4472e037df1d8fabec3498d1843
Cosdaman <Cosdaguy@gmail.com>
2021-11-20 12:56:17 -0800

    changed port to be heroku friendly

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const express = require('express');
 const routes = require('./routes/routes.js')
 
 //port
-const PORT = 3001;
+const PORT = process.env.PORT || 3001;
 
 //express setup
 const app = express();
