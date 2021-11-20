95e929a0c10eb29bdd333218c3d639dc26d9650f
Cosdaman <Cosdaguy@gmail.com>
2021-11-20 14:00:41 -0800

    added response to posts

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,8 +1,26 @@
 const Router = require('express').Router();
 const db = require('../db/db.json')
 
+
+
+//display notes
 Router.get('/api/notes', (req, res) => {
     res.status(200).json(db);
 });
+
+
+//create new notes
+Router.post('/api/notes', (req, res) => {
+    console.log(req.body)
+    let parse = JSON.parse(JSON.stringify(db))
+    parse.push(req.body)
+console.log("parse", parse)
+    // noteArr.push(req.body)
+    const response = {
+        status: 'success'
+    };
+    res.status(201).json(response);
+});
+
 
 module.exports = Router;
